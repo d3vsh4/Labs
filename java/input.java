6d71e43f5e5c076aaa5050225768f6e0d3bc7ea6
d3vsh4 <sdevanshu12345@gmail.com>
2019-02-04 03:29:23 -0600

    this is commit from lab

---
+++ java/input.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+class java {
+public static void main(String args[])throws Exception {
+	System.out.println("who are you");    
+	InputStreamReader SR= new InputStreamReader(System.in);
+	BufferedReader br = new BufferedReader(SR);
+	String Name = br.readLine();
+//print input
+	System.out.println("Hello! "+Name);
+	place();
+	}
+public static void place() { 
+	System.out.println("where are you");    
+	Scanner reader = new Scanner(System.in);
+	String name= reader.nextLine();
+//print input
+	System.out.println("wow!!! "+name+" is a nice place");
+	}
+}
+class matrixsorter {
+	System.out.println("Hello! "+Name);
+	}
